728e053afa06578ba9e819f874a3eecbb7bc3420
ke9000 <odakyu9000.8000ke@gmail.com>
2021-07-16 12:09:13 +0900

    [add]coreにメール共通function作った

--- core.php
+++ core.php
@@ -12,6 +12,8 @@ if($debug == "1"){
 	error_reporting(E_ALL);
 }
 
+$url = $ini['url'];
+
 /**
  * デバッグ
  * @param string $str デバッグ内容
@@ -125,5 +127,42 @@ function getAnsState(int $state){
 	}
 
 	return $str;
+}
+
+/**
+ * POSTでwebhookからメール通知する
+ * Webohook->G SpleedSheet ->Gmail
+ * 
+ * @param string $str メール通知する文章
+ * @return null
+ */
+function sendMailNotifer(string $str){
+	global $url;
+
+	$CURLERR = NULL;
+
+	$data = array(
+		'msg' => $str,
+	);
+
+	$ch = curl_init($url);
+
+	curl_setopt($ch, CURLOPT_POST, TRUE);                            //POSTで送信
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));    //データをセット
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);                    //受け取ったデータを変数に
+	$html = curl_exec($ch);
+
+	if(curl_errno($ch)){        //curlでエラー発生
+		$CURLERR .= 'curl_errno：' . curl_errno($ch) . "\n";
+		$CURLERR .= 'curl_error：' . curl_error($ch) . "\n";
+		$CURLERR .= '▼curl_getinfo' . "\n";
+		foreach(curl_getinfo($ch) as $key => $val){
+			$CURLERR .= '■' . $key . '：' . $val . "\n";
+		}
+		debug(nl2br($CURLERR));
+	}
+	curl_close($ch);
+
+	return null;
 }
 ?>
